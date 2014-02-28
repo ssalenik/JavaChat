d3c6a4fa58e2056765b920c06a04293ef0b00bc8
Stepan Salenikovich <stepan.salenikovich@gmail.com>
2014-02-27 21:17:10 -0500

    started stuff

---
+++ src/javachat/ServerCommunication.java
@@ -0,0 +1,15 @@
+package javachat;
+
+public class ServerCommunication {
+	final static int MSG_TYPE_LEN = 4;
+	final static int MSG_SUB_TYPE_LEN = 4;
+	final static int SIZE_LEN = 4;
+	
+	public static byte[] messageToByteArray(int messageType, int subMessageType, String messageData) {
+		byte[] byteMessage = null;
+		
+		
+		return byteMessage;
+	}
+
+}
